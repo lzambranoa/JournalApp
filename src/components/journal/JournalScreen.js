ebc8217dc0a92e2d7dfcb707589743e83534b472
lzambranoa <lzambranoa.software@gmail.com>
2021-01-20 21:19:05 -0500

    creacion del noteReducer

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Sidebar } from './Sidebar';
 import { NoteScreen } from '../notes/NoteScreen';
-// import { NothingSelected } from './NothingSelected';
+import { NothingSelected } from './NothingSelected';
+import { useSelector } from 'react-redux';
 
 
 export const JournalScreen = () => {
+
+    const {active} = useSelector( state => state.notes );
     return (
         <div className="journal__main-content">
             
@@ -12,9 +15,11 @@ export const JournalScreen = () => {
 
 
             <main>
-
-                {/* <NothingSelected /> */}
-                <NoteScreen />
+                {
+                    (active)
+                        ?(<NoteScreen />)
+                        :(<NothingSelected />)
+                }
 
             </main>
 
---
+++ src/reducers/notesReducer.js
@@ -0,0 +1,27 @@
+/*
+    {
+        notes: [],
+        active: null,
+        active: {
+            id: 'KAKJJAKJSAKLALSKWOW2W2EK',
+            title: '',
+            body: '',
+            imageUrl: '',
+            date: 566565123124545
+        }
+    }
+*/
+
+const initialState = {
+    notes: [],
+    active: null
+}
+
+export const notesReducer = (state = initialState, action) => {
+
+    switch(action.type){
+
+        default:
+            return state
+    }
+}
