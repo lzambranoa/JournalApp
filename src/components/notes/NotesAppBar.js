b64032ddf933d4c56ed5ae6a4b967d358e299f47
lzambranoa <lzambranoa.software@gmail.com>
2021-02-01 19:37:10 -0500

    Grabación de la nota en la base de datos

--- src/components/notes/NotesAppBar.js
+++ src/components/notes/NotesAppBar.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useDispatch, useSelector  } from 'react-redux';
+import { startSaveNote } from '../../actions/notes';
 
 export const NotesAppBar = () => {
+
+    const dispatch = useDispatch();
+    const {active} = useSelector( state => state.notes );
+    const handleSave = () => {
+        dispatch(startSaveNote( active ))
+    }
     return (
         <div className="notes__appbar">
             <span>28 de agosto 2020</span>
@@ -10,7 +18,9 @@ export const NotesAppBar = () => {
                     Picture
                 </button>
 
-                <button className="btn">
+                <button
+                onClick={ handleSave }
+                className="btn">
                     Save
                 </button>
             </div>
