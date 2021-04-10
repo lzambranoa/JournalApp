1e3aa050597850ec29b7ea041bae313376d6e90f
lzambranoa <lzambranoa.software@gmail.com>
2021-04-10 18:53:02 -0500

    docs agregado

---
+++ docs/precache-manifest.8e114caab98684f1c46dd411707f974e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d20e19b69381c1513c0f7020a3b8e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b415ff745e95ef79aa5",
+    "url": "/static/css/main.26922b4e.chunk.css"
+  },
+  {
+    "revision": "b6c5099c921838441b96",
+    "url": "/static/js/2.c776ee21.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/static/js/2.c776ee21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b415ff745e95ef79aa5",
+    "url": "/static/js/main.ca063935.chunk.js"
+  },
+  {
+    "revision": "1f9b80c10cb2174ac7b3",
+    "url": "/static/js/runtime-main.8c0f0de7.js"
+  }
+]);
